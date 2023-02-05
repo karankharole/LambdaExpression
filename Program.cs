cfcae98742ccfd26c8c2c16970f93a8f4f96d330
karankharole <karankharole2000@gmail.com>
2023-02-05 23:33:32 +0530

    recover top 2 aged person

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@
             personlist.Add(new Person("123426", "sagar", "surat", 15));
             personlist.Add(new Person("156657", "chetan", "Pune", 62));
             personlist.Add(new Person("758659", "karan", "ranchi", 20));
-           
+
+            Console.WriteLine("recovering Top 2  person from the list less than 60 years");
+            foreach (Person person in personlist.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
+            }
+
         }
     }
 }
