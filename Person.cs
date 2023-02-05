788af5563a7526f9ecec72bb1dcb18fe476976dd
karankharole <karankharole2000@gmail.com>
2023-02-05 19:14:12 +0530

    manage person details in list

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaExpression
+{
+    internal class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+        public Person(string ssn, string name, string adress, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = adress;
+            Age = age;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LamdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Person> personlist = new List<Person>();
+            personlist.Add(new Person("123654", "raaj", "akola", 22));
+            personlist.Add(new Person("123426", "sagar", "surat", 15));
+            personlist.Add(new Person("156657", "chetan", "Pune", 62));
+            personlist.Add(new Person("758659", "karan", "ranchi", 20));
+           
+        }
+    }
+}
